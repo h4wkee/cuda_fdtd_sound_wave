675ef86ebe57feda4d35be0a25d49832b8e5f3f5
h4wkee <j4strzab@gmail.com>
2020-05-10 13:21:28 +0200

    aaa

--- src/AcousticFDTD.cu
+++ src/AcousticFDTD.cu
@@ -228,10 +228,11 @@ void AcousticFDTD::draw()
 	/* Initial Waveform from a Point Source (1 pulse of sinusoidal wave with Hann window) */
 	if( _nPoint < (1.0/_freq)/_dt ){
 		_sigPoint = (1.0-cos((2.0*M_PI*_freq*_nPoint*_dt)))/2.0 * sin((2.0*M_PI*_freq*_nPoint*_dt));
-		//updatePoint<<<1, 1>>>(_gridSize, _grid[(int)_bufferSwap], _pointSource, _sigPoint);
+		updatePoint<<<1, 1>>>(_gridSize, _grid[(int)_bufferSwap], _pointSource, _sigPoint);
+		cudaDeviceSynchronize();
 	}
 
-	updateColors<<<_cudaGridSize, _cudaBlockSize>>>(_dataPerThread, _gridSize, _grid[(int)_bufferSwap], _vertexPointer);
+	//updateColors<<<_cudaGridSize, _cudaBlockSize>>>(_dataPerThread, _gridSize, _grid[(int)_bufferSwap], _vertexPointer);
 
 	cudaDeviceSynchronize();
 
