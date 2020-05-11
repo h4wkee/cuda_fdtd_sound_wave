a22163147e5ac3c01506932577bb19ca7bb7f2c2
h4wkee <j4strzab@gmail.com>
2020-05-11 18:11:15 +0200

    xxxx

--- src/AcousticFDTD.cu
+++ src/AcousticFDTD.cu
@@ -12,7 +12,7 @@ AcousticFDTD::AcousticFDTD(glm::ivec2 & gridSize, GLuint * vbo)
 
 	CudaSafeCall(cudaGraphicsGLRegisterBuffer(&_cudaVboRes, *vbo, cudaGraphicsMapFlagsNone));
 
-	_cudaBlockSize = dim3(gridSize.x / _dataPerThread.x, CUDA_THREADS_Y / _dataPerThread.y);
+	_cudaBlockSize = dim3(gridSize.x / _dataPerThread.x, gridSize.y / _dataPerThread.y);
 	const int bx = (gridSize.x + _cudaBlockSize.x - 1) / _cudaBlockSize.x;
 	const int by = (gridSize.y + _cudaBlockSize.y - 1) / _cudaBlockSize.y;
 	_cudaGridSize = dim3(bx, by);
